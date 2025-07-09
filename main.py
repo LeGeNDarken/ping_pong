c269e9f82a8df4c18c363fa54f24c4d5033fee38
LeGeNDarken <renatnurzhanov5@gmail.com>
2025-07-09 22:36:02 +0700

    added1

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 
-win = pygame.display.set_mode((700, 500))
+win = pygame.display.set_mode((600, 500))
 
 clock = pygame.time.Clock()
-
+back = (0, 220, 255)
 win.fill((0, 220, 255))
 
 is_game = True
@@ -22,11 +22,35 @@ class GameSprite(pygame.sprite.Sprite):
    def reset(self):
        win.blit(self.image, (self.rect.x, self.rect.y))
 
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y < 415:
+            self.rect.y += self.speed
+    def update_l(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[pygame.K_s] and self.rect.y < 415:
+            self.rect.y += self.speed
+
+
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
 while is_game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_game = False
 
     pygame.display.update()
+    racket1.reset()
+    racket2.reset()
+    ball.reset()
     clock.tick(40)
 #ДОПИШИ ИГРУ!
